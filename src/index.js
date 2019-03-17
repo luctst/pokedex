bee8cc8d908c257a926c05128c5a9253235b6bad
Lucas Tostée <lucas.tostee@gmail.com>
2019-03-17 05:06:29 +0100

    Add route for PokemonCard component

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import "./assets/scss/main.scss";
 import React from "react";
 import { render } from "react-dom";
 import {App} from "./view/App";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { PokeCard } from "./view/components/PokeCard";
 const app = document.querySelector("#root");
 
 /**
- * The global component of the app handle the the route here
+ * The global component of the app handle the route here
  */
 const Main = () => {
     return (
         <Router>
-            <Route exact path="/" component={App}/>
+            <Switch>
+                <Route exact path="/pokemon/:name" component={PokeCard}/>
+                <Route exact path="/" component={App}/>
+            </Switch>
         </Router>
     );
 }
