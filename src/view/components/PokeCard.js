0bd95537d88c27c61d946067d8ee33ddbadcbbf7
Lucas Tostée <lucas.tostee@gmail.com>
2019-03-17 05:07:08 +0100

    Start creating pokecard component which contains many data on a specific pokemon

--- src/view/components/PokeCard.js
+++ src/view/components/PokeCard.js
@@ -1,28 +1,91 @@
 /**
  * Import, Variables
  */
-import React, {useContext} from "react";
-import { Context } from "../../store/GlobalStore";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getData } from "../../actions/helper";
 
 /**
- * Render the pokemon card for only one pkmn
+ * Render the pokemon card with data on it.
  */
-export const PokeCard = () => {
-    const context = useContext(Context);
+export const PokeCard = props => {
+    const [state, setState] = useState({ pokemonData: null, dataFetched: false });
+    console.log(state);
 
-    if (context.pokemonData) {
-        return (
-            <section className="container">
-                <div className="row">
-                    <div className="col-12">        
-                        <div className="pokeCard">
-                            <h3 className="display-4">{context.pokemonData.name}</h3>
-                        </div>    
+    useEffect(() => {
+        try {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${props.match.params.name}`)
+                .then(data => data.json())
+                .then(dataParsed => setState({
+                    pokemonData: dataParsed,
+                    dataFetched: true
+                }));
+        } catch (error) {
+            throw error;
+        }
+    }, []);
+    const handlerType = (el, i) => {
+        switch (el.type.name) {
+            case "fire":
+                return <span key={i} className="badge badge-pill badge-danger">{el.type.name}</span>;
+                break;
+            case "flying":
+                return <span key={i} className="badge badge-pill badge-light">{el.type.name}</span>;
+                break;
+            case "electric":
+                return <span key={i} className="badge badge-pill badge-warning">{el.type.name}</span>;
+                break;
+            case "water":
+                return <span key={i} className="badge badge-pill badge-primary">{el.type.name}</span>;
+                break;
+            case "poison":
+                return <span key={i} className="badge badge-pill badge-secondary">{el.type.name}</span>;
+                break;
+            case "psychic":
+                return <span key={i} className="badge badge-pill badge-dark">{el.type.name}</span>;
+                break;
+            case "grass":
+                return <span key={i} className="badge badge-pill badge-success">{el.type.name}</span>;
+                break;
+            default:
+                break;
+        }
+    };
+
+    return (
+        <section className="container">
+            <div className="row">
+                {
+                    !state.dataFetched ?
+                        <div className="col-12 d-flex justify-content-center">We're fetching data.</div>
+                    : <div className="col-12 pokeCard">
+                        <div className="pokeCard--left">
+                            <h2>Name: {state.pokemonData.name}</h2>
+                            <p>Game id: <strong>{state.pokemonData.id}</strong></p>
+                            <p>Type: {
+                                state.pokemonData.types.map((el, i) => {
+                                    return handlerType(el, i);
+                                })
+                            }</p>
+                            <p>Occurs in:</p>
+                            <div className="pokeCard--left--version mb-3">
+                                {
+                                    state.pokemonData.game_indices.map((el, i) => {
+                                        return <span key={i} className="badge badge-info">{el.version.name}</span>
+                                    })
+                                }
+                            </div>
+                            <Link to="/" className="btn btn-success">Home</Link>
+                        </div>
+                        <div className="pokeCard--right">
+                            <img
+                                src={state.pokemonData.sprites.front_default}
+                                alt={`Picture of ${state.pokemonData.name}`}
+                            />
+                        </div>
                     </div>
-                </div>
-            </section>
-        );
-    } else {
-        return null;
-    }
+                };
+            </div>
+        </section>
+    );
 };
