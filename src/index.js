ec0fba0a060d680c28938bb98e4147bd66e6611e
kevin joya <kevin.joya@outlook.com>
2019-03-21 18:37:15 +0100

    Deleted all files to init project

--- src/index.js
+++ src/index.js
@@ -2,30 +2,6 @@
  * Import, Variables
  */
 import "./assets/scss/main.scss";
-import React from "react";
-import { render } from "react-dom";
-import {App} from "./view/App";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import { PokeCard } from "./view/components/PokeCard";
-import {PokeMove} from "./view/components/PokeMove";
+import App from "./view/app";
 const app = document.querySelector("#root");
 
-/**
- * The global component of the app handle the route here
- */
-const Main = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/moves/:name" component={PokeMove}/>
-                <Route exact path="/pokemon/:name" component={PokeCard}/>
-                <Route exact path="/" component={App}/>
-            </Switch>
-        </Router>
-    );
-}
-
-/**
- * Execute the render fonction
- */
-render(<Main/>, app);
--- src/view/app.js
+++ src/view/app.js
@@ -1,19 +1,5 @@
 /**
  * Import, Variables
  */
-import React from "react";
-import {GlobalStore} from "../store/GlobalStore";
-import {Header} from "./components/Header";
-import {Home} from "./container/Home";
 
-/**
- * The app component hold all the views available for our app.
- */
-export const App = () => {
-    return (
-        <GlobalStore>
-            <Header/>
-            <Home/>
-        </GlobalStore>
-    );
-}
+
