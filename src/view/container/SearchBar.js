411f4c6e328a27a25e9af55478605f03d8322106
kevin joya <kevin.joya@outlook.com>
2019-03-23 20:45:48 +0100

    Try to resolve bug

--- src/view/container/SearchBar.js
+++ src/view/container/SearchBar.js
@@ -4,10 +4,10 @@ export default class SearchBar {
     }
 
 
-/**
-* Render the searchbar and the icon filter
-@param {HTMLElement} element === the previous html element which is render.
-*/
+    /**
+    * Render the searchbar and the icon filter
+    @param {HTMLElement} element === the previous html element which is render.
+    */
     renderSearchBar(element) {
         const searchBar = document.createElement("div");
         searchBar.setAttribute('class', 'col-xs-3 search--bar');
@@ -18,24 +18,51 @@ export default class SearchBar {
         element.appendChild(searchBar);
         this.renderFilters();
     }
-    
+
     renderFilters() {
         const filterBtn = document.querySelector('.filter--btn');
+        const filters = document.createElement("div");
         filterBtn.addEventListener('click', () => {
-            const filters = document.createElement("div");
             filters.setAttribute('class', 'filters');
             filters.innerHTML = `
-            <ol>
-                <a class="filter-link" href="#"><li>Types</li></a>   
-                <a class="filter-link" href="#"><li>Hp</li></a>
-                <a class="filter-link" href="#"><li>rarity</li></a>
-                <a class="filter-link" href="#"><li>Retreat cost</li></a>
-                <a class="filter-link" href="#"><li>Weakness</li></a>
-                <a class="filter-link" href="#"><li>Resistance</li></a>
-                <a class="filter-link" href="#"><li>EvolvesFrom</li></a>
-            </ol>`;
+            <form>
+            <div class="form-row align-items-center">
+                <div class="col-auto my-1">
+                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                        <option selected>Choose...</option>
+                        <option value="1">Types</option>
+                        <option value="2">Hp</option>
+                        <option value="3">Rarity</option>
+                        <option value="4">Retreat cost</option>
+                        <option value="5">Weakness</option>
+                        <option value="6">Resistance</option>
+                        <option value="7">EvolvesFrom</option>
+                    </select>
+                </div>
+            </div>`;
             filterBtn.appendChild(filters)
-            
+        })
+
+        filterBtn.removeEventListener('click', () => {
+            filters.setAttribute('class', 'filters');
+            filters.innerHTML = `
+            <form>
+            <div class="form-row align-items-center">
+                <div class="col-auto my-1">
+                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                        <option selected>Choose...</option>
+                        <option value="1">Types</option>
+                        <option value="2">Hp</option>
+                        <option value="3">Rarity</option>
+                        <option value="4">Retreat cost</option>
+                        <option value="5">Weakness</option>
+                        <option value="6">Resistance</option>
+                        <option value="7">EvolvesFrom</option>
+                    </select>
+                </div>
+            </div>`;
         })
     }
 }
