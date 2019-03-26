7dd114f39cead3ce8d579efb3ea3b65c8aed6ddb
Lucas Tostée <lucas.tostee@gmail.com>
2019-03-26 01:23:59 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@
  * Import, Variables
  */
 import "./assets/scss/main.scss";
-import App from "./view/app";
-import PokeCards from "./view/container/PokeCards";
+import App from "./container/app";
+import Test from "./routes/Cards";
 const app = document.querySelector("#root");
 
-new App(app);
-
-if (window.location.href === "/") {
-    new App(app)
-} else if (window.location.href !== "/") {
-    window.pathname = 'pkmn_card.html';
-    console.log(window.pathname); 
+if (window.location.pathname === "/") {
+    new App(app);
+} else if (window.location.pathname === "/cards") {
+    new Test(app);
 }
