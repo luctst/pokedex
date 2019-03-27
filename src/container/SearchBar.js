34342e7c6f33124c46233437cccf20a1c3c1ca3b
kevin joya <kevin.joya@outlook.com>
2019-03-27 15:11:01 +0100

    Added method to search by name

--- src/container/SearchBar.js
+++ src/container/SearchBar.js
@@ -13,9 +13,28 @@ export default class SearchBar {
         searchBar.setAttribute('class', 'col-xs-3 search--bar');
         searchBar.innerHTML = `
         <input type="text" id="myInput" size="80" placeholder="Search by name">
-        <button class="button is-primary"><i class="fas fa-search"></i></button>`;
+        <button class="button is-primary search--btn"><i class="fas fa-search"></i></button>`;
         element.appendChild(searchBar);
         this.renderFilters(element);
+        this.searchByName();
+    }
+
+    /**
+     * Method to search by name and render all cards ask by the name.
+     */
+    searchByName() {
+        const btnSearch = document.querySelector('.search--btn');
+        const inputSearch = document.querySelector('input');
+        
+        btnSearch.addEventListener('click', () => {
+            window.location.href = `cards?name=${inputSearch.value}`;
+        })
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13) {
+                window.location.href = `cards?name=${inputSearch.value}`;
+            }
+        })
     }
 
     renderFilters(element) {
@@ -23,8 +42,8 @@ export default class SearchBar {
         const filters = document.createElement("div");
         filters.setAttribute('class', 'filters');
 
-       // filterBtn.addEventListener('click', () => {
-            filters.innerHTML = `
+        // filterBtn.addEventListener('click', () => {
+        filters.innerHTML = `
             <h3>Filters:</h3>
                 <label class="container">Trainers
                 <input type="checkbox">
@@ -61,7 +80,7 @@ export default class SearchBar {
                 <input type="checkbox">
                 <span class="checkmark"></span>
                 </label> `;
-       // })
+        // })
         element.appendChild(filters)
     }
 }
