7291025a119d4fba44561c49683aac8ccd29d1f5
kevin joya <kevin.joya@outlook.com>
2019-03-29 22:55:15 +0100

    clean code

--- src/container/PokeCards.js
+++ src/container/PokeCards.js
@@ -1,51 +1,22 @@
-import SearchBar from "./SearchBar";
-const testBar = new SearchBar(element);
-
-import Footer from "../components/Footer";
-
 export default class PokeCards {
     constructor(el) {
-        // this.pokemonData = [];
-        // this.getData(el)
         this.renderCards(el);
     }
 
-/**
- * Call API which pick up the data
- * RendeCards renders the cards's view.
- @param {HTMLElement} element === the previous html element which is render.
- */
-    // getData(element) {
-    //     fetch("https://api.pokemontcg.io/v1/cards?page=2")
-    //         .then(result => result.json())
-    //         .then(dataParsed => {
-    //             this.pokemonData = [...dataParsed.cards];
-    //             this.renderCards(element);
-    //         })
-    //         .catch(error => console.log(error));
-    // }
-
+    /**
+     * Call API which pick up the data
+     * RendeCards renders the cards's view.
+     @param {HTMLElement} element === the previous html element which is render.
+     */
     renderCards(element) {
         const cardsSection = document.createElement('section');
         cardsSection.setAttribute("class", "container-fluid d-flex flex-wrap mt-5 section--cards");
         this.pokemonData.map(el => {
-        const pokeCards = document.createElement('div');
-        pokeCards.setAttribute("class", "poke--cards");
+            const pokeCards = document.createElement('div');
+            pokeCards.setAttribute("class", "poke--cards");
             pokeCards.innerHTML = `<a class="card-link" href="${window.location.href}cards?name=${el.name}"><img class="card--picture" src="${el.imageUrl}"></a>`;
             cardsSection.appendChild(pokeCards);
-
-        /**
-         * @param {HTMLELement} el === the card clicked
-         * Test which pick up the el clicked
-         */
-        //    pokeCards.addEventListener('click', (event) => {
-        //        if (el.imageUrl === event.target.imageUrl) {
-        //         console.log(el.imageUrl);
-                    
-        //         }
-        //     })
         });
         element.appendChild(cardsSection);
-        new Footer(element);
     }
 }
