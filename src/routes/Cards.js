bb206fd96ef5c5547f929943d1a318912b7e7890
kevin joya <kevin.joya@outlook.com>
2019-03-29 22:56:09 +0100

    Refacto render and make loop attacks

--- src/routes/Cards.js
+++ src/routes/Cards.js
@@ -2,7 +2,6 @@
  * Import, variables..
  */
 import { getData } from "../actions/helper";
-
 /**
 * Render the /cards component. 
 */
@@ -16,9 +15,9 @@ export default class Cards {
      * @param {HTMLElement} el the html element to get with `render` function.
      */
     async getPokemonData(el) {
-        const pokeId = window.location.search.split("=");        
+        const pokeId = window.location.search.split("=");
         const dataParsed = await getData(`https://api.pokemontcg.io/v1/cards?id=${pokeId[1]}`);
-        
+
         if (dataParsed.cards.length === 0) {
             const section = document.createElement("section");
             section.setAttribute("class", "container d-flex justify-content-center align-items-center");
@@ -37,61 +36,51 @@ export default class Cards {
         }
     }
 
-    render(el, data) {
-        const section = document.createElement('section');
-        section.setAttribute("class", "container-fluid d-flex flex-wrap mt-5 section--cards");
-        section.innerHTML = `<h1>${data.cards[0].name}</h1>`;
-
-        const testId = data.cards[0].id;
-        console.log(testId); 
-
-        data.cards.map(el => {
-           // console.log(data.cards[0]);
-
-            const pokeCards = document.createElement('div');
-            pokeCards.setAttribute("class", "poke--cards");
-            pokeCards.innerHTML =
-                `<a class="card-link">
-                    <img class="card--picture" src="${el.imageUrl}">
-                </a>
-                <div class="about">
-                    <p>${el.supertype}</p>
-                    <p>${el.hp}</p>
-                    <p>${el.attacks}</p>
-                    <p>${el.convertedRetreatCost}</p>
-                    <p>${el.rarity}</p>
-                    <p>${el.retreatCost}</p>
-                    <p>${el.subtype}</p>
-                    <p>${el.types}</p>
-                    <p>${el.weaknesses}</p>
-                </div>`;
-            section.appendChild(pokeCards);
-            
-            /**
-             * @param {HTMLELement} el === the card clicked
-             * Test which pick up the el clicked
-             */
-            pokeCards.addEventListener('click', () => {
-                if (el.imageUrl) {
-                    console.log(el.imageUrl); 
-                } 
-            })
-            
-        });
-        el.appendChild(section);
+    loopAttacks(array) {
+        const attackArr = [];
+        array.map(el => {
+            Object.values(el).map(value => attackArr.push(value));
+        })
+        return attackArr;
     }
-    // <p>${this.loopAttacks(el.attacks)}</p>
-            // loopAttacks(array) {
-            //     for (let i = 0; i < array.length; i++) {
-            //        const  attacks = {
-            //             name: array[i].name, 
-            //             damage: array[i].damage,
-            //             text: array[i].text,
-            //             convertedEnergyCost: array[i].convertedEnergyCost
-            //         }
-            //         return Object.values(attacks);
-            //        // console.log(attacks);
-                    
-            //     }
-            // }
+
+    render(el, data) {
+       const valuesArr = this.loopAttacks(data.cards[0].attacks);
+       console.log(valuesArr);       
+       
+        el.innerHTML = `
+        <header class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>${data.cards[0].name}</h1>
+                </div>
+            </div>
+        </header>
+        <section class="container-fluid mt-5 section--cards">
+            <div class="row">
+                <div class="col-6 pkmn--img">
+                    <img class="pkmn--picture" src="${data.cards[0].imageUrl}">
+                </div>
+                <div class="col-6 pkmn--about">
+                    <p>${data.cards[0].supertype}</p>
+                    <p>${data.cards[0].hp}</p>
+                    <p>ATTACKS: </p>
+                    <div class="render--attacks"></div>
+                    <p>${data.cards[0].convertedRetreatCost}</p>
+                    <p>${data.cards[0].rarity}</p>
+                    <p>${data.cards[0].retreatCost}</p>
+                    <p>${data.cards[0].subtype}</p>
+                    <p>${data.cards[0].types}</p>
+                    <p>${data.cards[0].weaknesses}</p>
+                </div>
+            </div>
+        <section>`;
+
+        valuesArr.map(value => {
+            const htmlP = document.querySelector('.render--attacks');
+            const p = document.createElement('p');
+            p.textContent = value;
+            htmlP.appendChild(p);
+        })
+    }
 }
