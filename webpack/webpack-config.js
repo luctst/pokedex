f0e97b859f6a92af11e745da7b99fec36c267ca4
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-14 18:29:27 +0200

    Update webpack config

---
+++ webpack/webpack-config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+module.exports = filename => {
+    if (filename === "client") {
+        return {
+            mode: "development",
+            entry: path.resolve(`${filename}`, "index.js"),
+            output: {
+                path: path.resolve("docs"),
+                filename: "bundle.js"
+            },
+            devServer: {
+                contentBase: path.resolve(`${filename}`, "webpack-server"),
+                compress: true,
+                port: 8080,
+                open: true,
+                disableHostCheck: true,
+                historyApiFallback: true,
+            },
+        }
+    } else {
+        return {
+            mode: "development",
+            entry: path.resolve(`${filename}`, "index.js"),
+            output: {
+                path: path.resolve("docs", `${filename}`),
+                filename: "bundle.js"
+            },
+            devServer: {
+                contentBase: path.resolve(`${filename}`, "webpack-server"),
+                compress: true,
+                port: 8080,
+                open: true,
+                disableHostCheck: true,
+                historyApiFallback: true,
+            },
+        }
+    }
+}
