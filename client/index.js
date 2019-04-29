a42c7385376cb561cb774a4b91be77f8e701d491
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-30 01:44:27 +0200

    Remove switch statement

--- client/index.js
+++ client/index.js
@@ -1,17 +1,3 @@
 import App from "./container/App";
 const app = document.querySelector("#root");
-
-switch (window.location.pathname) {
-    case "/":
-        new App(app);
-        break;
-    case "/poke-cards":
-        const oldScript = document.querySelector("script");
-        const newScript = document.createElement("script");
-
-        newScript.setAttribute("src", "poke-cards/bundle.js");
-        document.querySelector("body").replaceChild(newScript, oldScript);
-        break;
-    default:
-        break;
-}
+App(app);
